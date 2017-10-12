069fc6146429af0c4e243124b10d0b8fdb53a769
Nathan Mertins <nmertins@ihmc.us>
2017-10-12 09:12:59 -0500

    first implementation for general polygon drawer

--- unit2/polygon-creation.js
+++ unit2/polygon-creation.js
@@ -27,10 +27,13 @@ function PolygonGeometry(sides) {
 
 		// YOUR CODE HERE
 		//Save the vertex location - fill in the code
-
+		geo.vertices.push(new THREE.Vector3(x, y, 0));
 	}
 	// YOUR CODE HERE
 	// Write the code to generate minimum number of faces for the polygon.
+	for (var i = 1; i < sides - 1; i++) {
+		geo.faces.push(new THREE.Face3(0, i, i + 1));
+	}
 
 	// Return the geometry object
 	return geo;
