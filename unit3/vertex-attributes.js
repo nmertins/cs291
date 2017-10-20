7b75dba588ea7d5679a3ed081cf770ee2c2e81df
Nathan Mertins <nmertins@ihmc.us>
2017-10-20 09:28:18 -0500

    implementation for triangle with color interpolation from each vertex

--- unit3/vertex-attributes.js
+++ unit3/vertex-attributes.js
@@ -14,7 +14,17 @@ function fillScene() {
 	geometry = new THREE.Geometry();
 
 	// Student: add a colored triangle here
+	geometry.vertices.push(new THREE.Vector3(100, 0, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 100, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 0, 100));
 
+	geometry.faces.push(new THREE.Face3(0, 1, 2));
+
+	var color1 = new THREE.Color(0xFF0000);
+	var color2 = new THREE.Color(0x00FF00);
+	var color3 = new THREE.Color(0x0000FF);
+
+	geometry.faces[0].vertexColors = [color1, color2, color3];
 
 	mesh = new THREE.Mesh( geometry, material );
 
